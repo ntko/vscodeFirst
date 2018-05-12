8c084f2e66379549407322d626a4620a8cce258e
Tanger John <ntko@users.noreply.github.com>
2018-05-13 03:42:39 +0800

    config auto build and debug vscode file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+using namespace std
+;
+
+class firstClass
+{
+public:
+  firstClass()
+  {
+    m_inti = 10;
+  }
+  int get_int()
+  {
+    return m_inti;
+  }
+  void set_intM3(int i)
+  {
+    m_inti = i * 3;
+  }
+
+private:
+  int m_inti;
+};
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
-  std::cout << "ok ok ok!" << std::endl;
+  firstClass fc;
+
+  cout << "ok ok ok! m_inti=" << fc.get_int() << endl;
+
+  fc.set_intM3(20);
+
+  cout << "ok ok ok! m_inti=" << fc.get_int() << endl;
+
   return 0;
 }
